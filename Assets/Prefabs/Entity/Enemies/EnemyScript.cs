3d2a11552d8ac96ae788ed3e97da75881eb25928
TenebrisCattus <dimasu5555@gmail.com>
2026-07-24 12:54:36 +0300

    Увеличил управляемость в воздухе и начал перелопачивать врага.

--- Assets/Prefabs/Entity/Enemies/EnemyScript.cs
+++ Assets/Prefabs/Entity/Enemies/EnemyScript.cs
@@ -3,9 +3,12 @@ using UnityEngine.EventSystems;
 
 public class EnemyScript : EntityScript
 {
-    public Transform target; // Объект, до которого проверяем видимость
-    public LayerMask obstacleLayer; // Слой, на котором находятся стены и препятствия
+    private LayerMask obstacleLayer;
 
+    private void Start()
+    {
+        obstacleLayer = LayerMask.NameToLayer("Default");
+    }
     void Update()
     {
         Debug.Log(HasLineOfSight());
@@ -13,18 +16,13 @@ public class EnemyScript : EntityScript
 
     bool HasLineOfSight()
     {
-        // Выпускаем луч от текущего объекта до цели
-        RaycastHit2D hit = Physics2D.Linecast(transform.position, target.position, obstacleLayer);
-
-        // Если луч ничего не встретил на своем пути, значит, преград нет
+        RaycastHit2D hit = Physics2D.Linecast(transform.position, PlayerScript.Game_player.transform.position, obstacleLayer);
         if (hit.collider == null)
         {
-            Debug.DrawLine(transform.position, target.position, Color.green);
             return true; // Прямая видимость есть
         }
         else
         {
-            Debug.DrawLine(transform.position, hit.point, Color.red);
             return false; // На пути есть препятствие
         }
     }
